b0634d0a0525f810ce77016034682fdf1fb952e4
JyZo <jjy3680@naver.com>
2024-01-27 23:03:11 +0900

    footer default set

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './footer.scss';
 import { Link } from 'react-router-dom';
 
-const footer = () => {
+const Footer = () => {
+  const [isSwitched, setIsSwitced] = useState(false);
+  const switchHandler = () => {
+    console.log(isSwitched);
+
+    setIsSwitced(!isSwitched);
+  };
+
   return (
     <footer className="footer_root">
       <div className="footer_wrap_item">
@@ -65,10 +72,7 @@ const footer = () => {
                       width="1em"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
-                        fill-rule="evenodd"
-                        d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189 221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15 4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31z"
-                      ></path>
+                      <path d="M508.64 148.79c0-45-33.1-81.2-74-81.2C379.24 65 322.74 64 265 64h-18c-57.6 0-114.2 1-169.6 3.6C36.6 67.6 3.5 104 3.5 149 1 184.59-.06 220.19 0 255.79q-.15 53.4 3.4 106.9c0 45 33.1 81.5 73.9 81.5 58.2 2.7 117.9 3.9 178.6 3.8q91.2.3 178.6-3.8c40.9 0 74-36.5 74-81.5 2.4-35.7 3.5-71.3 3.4-107q.34-53.4-3.26-106.9zM207 353.89v-196.5l145 98.2z"></path>
                     </svg>
                   </span>
                   유튜브
@@ -84,10 +88,7 @@ const footer = () => {
                       width="1em"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
-                        fill-rule="evenodd"
-                        d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189 221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15 4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31z"
-                      ></path>
+                      <path d="M508.64 148.79c0-45-33.1-81.2-74-81.2C379.24 65 322.74 64 265 64h-18c-57.6 0-114.2 1-169.6 3.6C36.6 67.6 3.5 104 3.5 149 1 184.59-.06 220.19 0 255.79q-.15 53.4 3.4 106.9c0 45 33.1 81.5 73.9 81.5 58.2 2.7 117.9 3.9 178.6 3.8q91.2.3 178.6-3.8c40.9 0 74-36.5 74-81.5 2.4-35.7 3.5-71.3 3.4-107q.34-53.4-3.26-106.9zM207 353.89v-196.5l145 98.2z"></path>
                     </svg>
                   </span>
                   카페
@@ -123,8 +124,8 @@ const footer = () => {
               구매 회원 간의 상품거래 정보 및 거래에 관여하지 않고, 어떠한
               의무와 책임도 부담하지 않습니다.
             </p>
-            <div className="catebutton">
-              <button>
+            <div className={`${isSwitched ? 'catebuttonOn' : 'catebuttonOff'}`}>
+              <button onClick={switchHandler}>
                 카테고리 리스트
                 <svg
                   stroke="currentColor"
@@ -185,4 +186,4 @@ const footer = () => {
   );
 };
 
-export default footer;
+export default Footer;
