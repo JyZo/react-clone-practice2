43423cee73a9d45a428deaf1f3d71dcbe605c2db
JyZo <jjy3680@naver.com>
2024-02-07 03:52:20 +0900

    top main ING

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './main.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const main = () => {
   return (
     <main className="main_root">
-      <div className="main1">MAIN</div>
-      <div className="main1">MAIN2</div>
+      <div className="top_main">
+        <div className="carousel_wrapper">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            slidesPerView={3}
+            spaceBetween={30}
+            navigation
+            pagination={{ clickable: true }}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+      <div className="bot_main">MAIN2</div>
     </main>
   );
 };
