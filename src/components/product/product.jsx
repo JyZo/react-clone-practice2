44b9e9f880614a7f9e4d385fbc7fc6831f946038
JyZo <jjy3680@naver.com>
2024-02-22 01:30:17 +0900

    recommand swiper default set

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -3,27 +3,20 @@ import './product.scss';
 import NaverPay from '../svg/naverpay';
 
 const product = ({ imgSrc, name, price, location, timer, paymentSrc }) => {
-  console.log(paymentSrc);
-
   return (
     <>
-      <div>
-        <img
-          alt="아이폰"
-          src={imgSrc}
-          // decoding="async"
-          // data-nimg="fill"
-        ></img>
+      <div className="product_img">
+        <img alt="아이폰" src={imgSrc}></img>
       </div>
-      <div class="">
-        <h2 class="">{name}</h2>
-        <div class="">{price}</div>
-        <div class="">
+      <div class="product_info">
+        <h2 class="product_name">{name}</h2>
+        <div class="product_price">{price}</div>
+        <div class="product_lo_ti">
           <span class="">{location}</span>
           <span class="">|</span>
           <span class="">{timer}</span>
         </div>
-        <div class="">{paymentSrc === 'true' ? <NaverPay /> : 'false'}</div>
+        <div class="">{paymentSrc === 'true' ? <NaverPay /> : '\u00A0'}</div>
       </div>
     </>
   );
